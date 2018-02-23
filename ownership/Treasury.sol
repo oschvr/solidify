b21a2b4f097d2c7efa0980cdda882ff44292a911
oschvr <oscarchavezromero@gmail.com>
2018-02-22 21:52:06 -0600

    Add Treasury to ownership

---
+++ ownership/Treasury.sol
@@ -0,0 +1,52 @@
+/*
+ * Treasury is an ownership set of rules for smart contracts
+ * Smart Contract written in Solidity 0.4.18
+ * 
+ * Author: Oscar Chavez <oschvr@gmail.com>
+ * Last Updated: 22/02/2018
+ * v 0.1
+ *
+ */
+
+pragma solidity ^0.4.18;
+
+/**
+ * @title Treasury
+ * @dev Treasury contract has an owner address, provides basic authorization control
+ */
+contract Treasury {
+	address public treasury;
+
+	/*
+	 * @dev Event to log the transfer of ownership
+ 	 */
+	event TreasuryTransferred(address indexed previousOwner, address indexed newOwner);
+	
+	/*
+	 * @dev Throws if called is not account of owner.
+	 */
+	modifier ifTreasury() {
+		require(msg.sender == treasury);
+		_;
+	}
+
+	/*
+	 * @dev Treasury constructor. Sets the original owner to the sender account.
+	 */
+	function Treasury() public {
+		treasury = msg.sender;
+	}
+
+	/*
+	 * @dev Allows current owner to transfer control to newOwner.
+	 * @param newOwner The address to receive control.	
+	 */
+	function transferTreasury(address newTreasury) public ifTreasury {
+		require(newTreasury != address(0));
+		TreasuryTransferred(treasury, newTreasury);
+		treasury = newTreasury;
+	}
+}
+
+
+
